1a05afd078c425d665f7ad44f17742232e6e961a
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-07-10 00:59:36 +0200

    style: :lipstick: Tailwindcss configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind'
+
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://happydevs-blog.netlify.app",
-  integrations: [preact()]
+  integrations: [preact(), tailwind()]
 });
